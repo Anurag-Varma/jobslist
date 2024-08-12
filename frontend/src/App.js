9182feef6cb3b221f32221cad477d0ce29b22c62
Anurag Varma <panuragvarma123456789@gmail.com>
2024-08-12 14:43:16 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,36 +3,43 @@ import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 import Cookies from 'js-cookie';
+import { useRecoilValue } from 'recoil';
 import userAtom from './atoms/userAtom';
 
 function App() {
   const [user, setUser] = useState(null);
   const currentUser = useRecoilValue(userAtom);
 
-  useEffect(() => {
-    const checkAuthStatus = () => {
-      const jwtToken = Cookies.get('jwt');
-      if (jwtToken) {
-        // If the cookie is present, use the value from Recoil
-        setUser(currentUser);
-      } else {
-        // If the cookie is not present, set user to null
-        setUser(null);
-      }
-    };
+  const updateUserState = () => {
+    const jwtToken = Cookies.get('jwt');
+    if (jwtToken) {
+      // If the cookie is present, use the value from Recoil
+      setUser(currentUser);
+    } else {
+      // If the cookie is not present, set user to null
+      setUser(null);
+    }
+  };
 
+  useEffect(() => {
     // Initial check on component mount
-    checkAuthStatus();
+    updateUserState();
 
     // Periodic check every 60 seconds
     const intervalId = setInterval(() => {
-      checkAuthStatus();
+      updateUserState();
     }, 60000);
 
-    // Clean up the interval on component unmount
-    return () => clearInterval(intervalId);
+    // Event listener for storage changes (handles changes in other tabs)
+    window.addEventListener('storage', updateUserState);
+
+    // Clean up the interval and event listener on component unmount
+    return () => {
+      clearInterval(intervalId);
+      window.removeEventListener('storage', updateUserState);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUser]);
 
   return (
