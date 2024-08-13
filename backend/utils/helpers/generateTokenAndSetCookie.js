ff3644e49a95a94e0b1e6ef9494b54bd3a0fec53
Anurag Varma <panuragvarma123456789@gmail.com>
2024-08-14 01:34:38 +0530

    Update generateTokenAndSetCookie.js

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -1,19 +1,19 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 const generateTokenAndSetCookie = (userId, res) => {
-
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: '3d'
     });
 
     res.cookie('jwt', token, {
-        secure: true,
-        sameSite: 'strict',
-        maxAge: 1000 * 60 * 60 * 24 * 3
+        domain: '.jobslist.live', // Ensure the cookie is available to all subdomains
+        path: '/',                // Make the cookie available across the entire domain
+        secure: true,             // Ensure the cookie is sent over HTTPS
+        sameSite: 'None',         // Allow cross-origin requests to include the cookie
+        maxAge: 1000 * 60 * 60 * 24 * 3 // Cookie expiration time
     });
 
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-
 }
 
 export { generateTokenAndSetCookie }
